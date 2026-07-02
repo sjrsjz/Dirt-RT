7583ec6e41f8c42bbc33f78ec0375779451a37cd
sjrsjz <3244658766@qq.com>
2026-07-02 18:07:52 +0800

    fix: final composite

--- shaders/post/fog.glsl
+++ shaders/post/fog.glsl
@@ -83,7 +83,7 @@ void main() {
                       + data.emission;
         // // 调试输出: 直接输出各分量的线性组合，验证时域累积效果
         // fragColor.xyz = project_SH_irradiance(tmp.data_swap, decodeNormal(diffuseIllumiantionBuffer.data[idx].oct_n2));
-        fragColor.xyz = tmp2.data_swap * data.albedo;
+        // fragColor.xyz = tmp2.data_swap * data.albedo;
         // fragColor.xyz = normalize(abs(tmp.data_swap.shY.xyz));
         // fragColor.xyz = vec3(alice_estimator_variance(tmp.data_swap.shY, tmp.weight));
         // fragColor.xyz = vec3(tmp.weight);
